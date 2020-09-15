8437ed9edc87b8c2983388759a6279b2d1681dc0
Stedag <stephen.dages@gmail.com>
2020-09-15 10:07:29 -0400

    Added API reference URL for convenience.

--- at_a_glance.py
+++ at_a_glance.py
@@ -1,6 +1,8 @@
 import requests
 import json
 
+# https://wikimedia.org/api/rest_v1/#/
+
 while True:
     var =input("Enter query below (or \"quit\" to quit):").strip()
     r=requests.get("/".join(["https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article","en.wikipedia.org","all-access", "all-agents",var,"monthly/20200101/20200909"]))
