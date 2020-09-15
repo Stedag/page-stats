534937d2155c06c7c284fcd304de1eb37be0573b
Stedag <stephen.dages@gmail.com>
2020-09-15 09:06:37 -0400

    adding a script which retreives five days of page viewership

---
+++ at_a_glance.py
@@ -0,0 +1,14 @@
+import requests
+import json
+
+while True:
+    var =input("Enter query below (or \"quit\" to quit):").strip()
+    r=requests.get("/".join(["https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article","en.wikipedia.org","all-access", "all-agents",var,"monthly/20200101/20200909"]))
+    if var=="quit":
+        break
+    print("last week of daily pageviews for ",var,":")
+    try:
+        for x in json.loads(r.content)["items"]:
+            print (x["views"])
+    except:
+        print("read error")
